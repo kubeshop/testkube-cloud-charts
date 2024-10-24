df394ac6bd04924b355129e6fa0729e288d9eb82
Kubeshop <174873053+ed382@users.noreply.github.com>
2024-10-24 21:50:16 +0200

    feat: lint images to check that .global.imageRegistry is used on all detected images

---
+++ scripts/image_check.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+REPO=myownrepo.com/prefix
+CP_IMAGES=images.txt
+
+# Build the dependencies
+helm dependency build ../charts/testkube-enterprise
+
+# Get images from the control plane chart
+helm template test testkubeenterprise/testkube-enterprise --skip-crds --set global.imageRegistry=$REPO --set global.certificateProvider="" --set global.testWorkflows.createOfficialTemplates=false | grep "image:" | grep -v "{" | sed 's/"//g' | sed 's/docker.io\///g' | awk '{ print $2 }' | awk 'NF && !seen[$0]++' | sort > "$CP_IMAGES"
+
+# Check for images that do not start with the image registry
+failure=false
+while IFS= read -r line; do
+    if [[ ! "$line" =~ ^"$REPO" ]]; then
+        echo "Failure: Line '$line' does not start with '$REPO'."
+        failure=true
+    fi
+done < "$CP_IMAGES"
+
+if [ "$failure" = true ]; then
+    exit 1
+fi
+
+echo "All lines start with '$REPO'."
