66b8ded5d9e41c97420502a27d90da083c104dbc
Kubeshop <174873053+ed382@users.noreply.github.com>
2024-10-28 19:28:11 +0100

    fix: remove repos

--- scripts/image_check.sh
+++ scripts/image_check.sh
@@ -3,11 +3,6 @@
 REPO=myownrepo.com/prefix
 CP_IMAGES=images.txt
 
-# Add repos
-helm repo add bitnami https://charts.bitnami.com/bitnami
-helm repo add kubeshop https://kubeshop.github.io/helm-charts
-helm repo add nats https://nats-io.github.io/k8s/helm/charts/
-
 # Build the dependencies
 helm dependency build ../charts/testkube-enterprise
 
